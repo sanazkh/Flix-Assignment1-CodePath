53266ae22f4464ed5cf696e6dcc8870e13d34bf3
sanazkh <sanaz.khosravi70@gmail.com>
2018-09-17 11:11:55 -0700

    some auto layout

--- Flixo/Controllers/NowPlayingViewController.swift
+++ Flixo/Controllers/NowPlayingViewController.swift
@@ -5,14 +5,13 @@
 //  Created by Sanaz Khosravi on 9/6/18.
 //  Copyright © 2018 GirlsWhoCode. All rights reserved.
 //
-
 import UIKit
 import AlamofireImage
 import AFNetworking
 import MBProgressHUD
 
 class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
-
+    
     @IBOutlet var mySearchBar: UISearchBar!
     var movies : [[String: Any]] = []
     var allMovies : [[String: Any]] = []
@@ -29,26 +28,28 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         myTableView.insertSubview(refreshControl, at: 0)
         myTableView.dataSource = self
         myTableView.delegate = self
-        myTableView.rowHeight = 240
+        // self sizing row
+        myTableView.rowHeight = UITableViewAutomaticDimension
+        myTableView.estimatedRowHeight = 150
         fetchNowPlayingMoview()
         
     }
     
-   @objc func refreshControlAction(_ refreshControl: UIRefreshControl) {
+    @objc func refreshControlAction(_ refreshControl: UIRefreshControl) {
         fetchNowPlayingMoview()
-    
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
+        
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // return filterMovies.count
         return movies.count
     }
-
+    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MovieCell
         let movie = movies[indexPath.row]
@@ -56,16 +57,22 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         let overview = movie["overview"] as! String
         
         cell.myTitleLabel.text = title
+        cell.myTitleLabel.sizeToFit()
+        
+        cell.myTitleLabel.adjustsFontSizeToFitWidth = true
         cell.myDescLabel.text = overview
+        
+        cell.myDescLabel.sizeToFit()
+        cell.myDescLabel.adjustsFontSizeToFitWidth = true
         let placeholderImage = UIImage(named: "default-placeholder.png")!
-      /*  if let posterPathString = movie["poster_path"] as? String {
-            let baseURlString = "https://image.tmdb.org/t/p/w500"
-            let posterURL = URL(string: baseURlString + posterPathString)!
-            // Setting image placeholder
-            cell.myImageView.af_setImage(withURL: posterURL, placeholderImage: placeholderImage, imageTransition: .flipFromBottom(2))
-        }else{
-            cell.myImageView.image = placeholderImage
-        } */
+        /*  if let posterPathString = movie["poster_path"] as? String {
+         let baseURlString = "https://image.tmdb.org/t/p/w500"
+         let posterURL = URL(string: baseURlString + posterPathString)!
+         // Setting image placeholder
+         cell.myImageView.af_setImage(withURL: posterURL, placeholderImage: placeholderImage, imageTransition: .flipFromBottom(2))
+         }else{
+         cell.myImageView.image = placeholderImage
+         } */
         
         if let posterPathString = movie["poster_path"] as? String {
             let baseURLStringSmall = "https://image.tmdb.org/t/p/w45"
@@ -82,29 +89,25 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
                 placeholderImage: nil,
                 success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
                     
-                        // smallImageResponse will be nil if the smallImage is already available
-                        // in cache
-                        cell.myImageView.alpha = 0.0
-                        cell.myImageView.image = smallImage;
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache
+                    cell.myImageView.alpha = 0.0
+                    cell.myImageView.image = smallImage;
+                    UIView.animate(withDuration: 0.3, animations: { () -> Void in
                         
-                        UIView.animate(withDuration: 0.3, animations: { () -> Void in
-                            
-                            cell.myImageView.alpha = 1.0
-                            
-                        }, completion: { (sucess) -> Void in
-                            
-                            // The AFNetworking ImageView Category only allows one request to be sent at a time
-                            // per ImageView. This code must be in the completion block.
-                            cell.myImageView.setImageWith(
-                                largeImageRequest,
-                                placeholderImage: smallImage,
-                                success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
-                                         cell.myImageView.image = largeImage;
-                            },
-                                failure: { (request, response, error) -> Void in
-                                    cell.myImageView.image = placeholderImage
-                            })
+                        cell.myImageView.alpha = 1.0
+                        
+                    }, completion: { (sucess) -> Void in
+                        cell.myImageView.setImageWith(
+                            largeImageRequest,
+                            placeholderImage: smallImage,
+                            success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                                cell.myImageView.image = largeImage;
+                        },
+                            failure: { (request, response, error) -> Void in
+                                cell.myImageView.image = placeholderImage
                         })
+                    })
                     
             },
                 failure: { (request, response, error) -> Void in
@@ -117,7 +120,7 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         cell.selectedBackgroundView = backgroundView
         return cell
     }
-
+    
     
     func fetchNowPlayingMoview(){
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=85c378ca43ad66ca8fa593bb2aaacca0")!
@@ -125,7 +128,6 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         MBProgressHUD.showAdded(to: self.view, animated: true)
         let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
             if let error = error {
                 let alert = UIAlertController(title: "Error!", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
                 alert.addAction(UIAlertAction(title: "Try Again", style: .default, handler:nil))
@@ -151,7 +153,7 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
             
             let res = item["title"] as! String
             return res.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
-            }
+        }
         self.myTableView.reloadData()
     }
     
@@ -191,5 +193,6 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         myTableView.reloadData()
     }
 }
+
 
 
--- Flixo/MovieAPIManager.swift
+++ Flixo/MovieAPIManager.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+class MovieAPIManager {
+    
+    static let baseUrl = "https://api.themoviedb.org/3/movie/"
+    static let apiKey = "85c378ca43ad66ca8fa593bb2aaacca0"
+    var session: URLSession
+    
+    init() {
+        session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+    }
+    
+    func nowPlayingMovies(completion: @escaping ([Movie]?, Error?) -> ()) {
+        let url = URL(string: MovieAPIManager.baseUrl + "now_playing?api_key=\(MovieAPIManager.apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movieDictionaries = dataDictionary["results"] as! [[String: Any]]
+                
+                let movies = Movie.movies(dictionaries: movieDictionaries)
+                completion(movies, nil)
+            } else {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
+    
+    
+
+
+    
+    func superheroMovies(completion: @escaping ([Movie]?, Error?) -> ()) {
+        let url = URL(string: MovieAPIManager.baseUrl + "141052/similar?api_key=\(MovieAPIManager.apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movieDictionaries = dataDictionary["results"] as! [[String: Any]]
+                
+                let movies = Movie.movies(dictionaries: movieDictionaries)
+                completion(movies, nil)
+            } else {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
+}
+
+
